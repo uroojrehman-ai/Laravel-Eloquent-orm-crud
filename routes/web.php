b7943eb7ac649cc4649d8dc8b380844f1528310e
uroojrehman-ai <uroojrehman825@gmail.com>
2026-03-12 23:05:13 +0500

    user data retreive

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+//session1
 Route::get("/insert",[StudentController::class,'insertData']);
 Route::get("/get",[StudentController::class,'getall']);
 Route::get("/getsingle/{id}",[StudentController::class,'getsinglerecord']);
 Route::get("/update/{id}",[StudentController::class,'update']);
 Route::get("/delete/{id}",[StudentController::class,'deleterecord']);
+
+//session2
+Route::get("/user/getall",[UserController::class,'getall']);
